dc45b816ad978c413a2a44cded4306b1d49228d9
Jhonattan Lino <63247251+Jhonattanln@users.noreply.github.com>
2020-11-10 13:14:34 -0300

    Update Moral_hazard.py

--- Moral hazard/Moral_hazard.py
+++ Moral hazard/Moral_hazard.py
@@ -10,3 +10,28 @@ from statsmodels import regression
 macro = pd.read_excel(r'Macroecomics indicators.xlsx', parse_dates=True, index_col=0)
 pop = pd.read_excel(r'Popularity.xlsx', parse_dates=True, index_col='DATA')
 
+#### Manipulating time series
+
+macroQ = macro.asfreq('Q', method='ffill')
+popQ = pop.asfreq('Q', method='ffill')
+macroQ
+
+serieI = pd.concat([macroQ, popQ['POSITIVA']], axis=1).dropna()
+serieI
+
+#### Linear regression
+
+def reg (X, Y):
+    X = sm.add_constant(X)
+    model = regression.linear_model.OLS(Y, X).fit()
+    return model.summary()
+
+reg(serieI['Dívida/PIB - %'], serieI['POSITIVA'])
+
+
+plt.style.use('ggplot')
+sns.regplot(data=serieI, 
+            x='Dívida/PIB - %',
+            y='POSITIVA')
+plt.ylabel('Avaliação positiva')
+plt.show()
